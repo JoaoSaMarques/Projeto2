207ab68402287b1a7dbf6cc8dcc6507308039f25
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-23 13:55:02 +0100

    Fix IRecipe and IComparable not being implemented

--- CraftingSim/Model/Recipe.cs
+++ CraftingSim/Model/Recipe.cs
@@ -5,27 +5,27 @@ using System.Threading.Tasks;
 
 namespace CraftingSim.Model
 {
-    public class Recipe : IRecipe, IComparable<Recipe>
+    public class Recipe : IRecipe, IComparable<IRecipe>
     {
         public string Name { get; }
         public double SuccessRate { get; }
         public IReadOnlyDictionary<IMaterial, int> RequiredMaterials { get; }
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="successRate"></param>
-        /// <param name="requiredMaterials"></param>
-        public Recipe(string name, double successRate, Dictionary<IMaterial,
-        int> requiredMaterials)
+         /// <summary>
+         /// Constructor
+         /// </summary>
+         /// <param name="name"></param>
+         /// <param name="successRate"></param>
+         /// <param name="requiredMaterials"></param>
+        public Recipe(string name, double successRate,
+        Dictionary<IMaterial, int> requiredMaterials)
         {
             Name = name;
             SuccessRate = successRate;
             RequiredMaterials = new Dictionary<IMaterial, int>(requiredMaterials);
         }
 
-        public int CompareTo(Recipe other)
+        public int CompareTo(IRecipe other)
         {
             if (other == null)
                 return 1;

670cf5d0a99469f6cef69274e9511845e178cc31
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-05-23 13:10:58 +0100

    create CraftingSim

---
+++ CraftingSim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CraftingSim
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
